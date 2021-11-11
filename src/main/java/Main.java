ada6ec660b34fdbf06101d85258d1fba04d0047d
blazej <blazejp.bp@gmail.com>
2021-11-11 01:36:43 +0100

    Poprawki.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,25 +9,6 @@ public class Main {
 
   public static void main(String[] args) throws Exception {
 
-    Task task0 = new Task(
-        new Input(
-            Lists.newArrayList(
-                new Point(4d, 4d),
-                new Point(2.2d, 10d),
-                new Point(0d, 6d),
-                new Point(2.1d, 5d),
-                new Point(1d, 1.2d),
-                new Point(2d, 2d),
-                new Point(4.1d, 1.1d),
-                new Point(10.1d, 2.5d),
-                new Point(10d, 3.5d),
-                new Point(5d, 5.1d)
-            ),
-            new Region(0.0, 10.0, 0.0, 10.0)
-        ),
-        new Region(2.0d, 4.0d, 2.0d, 4.0d)
-    );
-
     Task task10 = new Task(
         new InputBuilder(10).build(),
         new Region(
